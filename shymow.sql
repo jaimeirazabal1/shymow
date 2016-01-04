02d20601106042af3b054291309951b3269d4f84
jaimeirazabal1 <jaimeirazabal1@gmail.com>
2016-01-04 16:20:35 -0430

    agregando las provincias1

--- shymow.sql
+++ shymow.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Tiempo de generación: 29-12-2015 a las 19:25:37
+-- Tiempo de generación: 04-01-2016 a las 16:20:01
 -- Versión del servidor: 5.5.46-0ubuntu0.14.04.2
 -- Versión de PHP: 5.5.9-1ubuntu4.14
 
@@ -38,6 +38,94 @@ CREATE TABLE IF NOT EXISTS `avatar` (
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `ciudad`
+--
+
+CREATE TABLE IF NOT EXISTS `ciudad` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `createdAt` datetime DEFAULT NULL,
+  `updatedAt` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ciudades`
+--
+
+CREATE TABLE IF NOT EXISTS `ciudades` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ciudad` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `comunidad_automona`
+--
+
+CREATE TABLE IF NOT EXISTS `comunidad_automona` (
+  `comunidad` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `createdAt` datetime DEFAULT NULL,
+  `updatedAt` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `comunidad` (`comunidad`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=20 ;
+
+--
+-- Volcado de datos para la tabla `comunidad_automona`
+--
+
+INSERT INTO `comunidad_automona` (`comunidad`, `id`, `createdAt`, `updatedAt`) VALUES
+('Andalucía', 1, NULL, NULL),
+('Aragón', 2, NULL, NULL),
+('Principado de Asturias', 3, NULL, NULL),
+('Islas Baleares', 4, NULL, NULL),
+('Canarias', 5, NULL, NULL),
+('Cantabria', 6, NULL, NULL),
+('Castilla y León', 7, NULL, NULL),
+('Castilla – La Mancha', 8, NULL, NULL),
+('Cataluña', 9, NULL, NULL),
+('Comunidad Valenciana', 10, NULL, NULL),
+('Extremadura', 11, NULL, NULL),
+('Galicia', 12, NULL, NULL),
+('Comunidad de Madrid', 13, NULL, NULL),
+('Murcia', 14, NULL, NULL),
+('Navarra', 15, NULL, NULL),
+('País Vasco', 16, NULL, NULL),
+('La Rioja', 17, NULL, NULL),
+('Ceuta (Ciudad Autónoma de)', 18, NULL, NULL),
+('Melilla (Ciudad Autónoma de)', 19, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `provincia`
+--
+
+CREATE TABLE IF NOT EXISTS `provincia` (
+  `provincia` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `createdAt` datetime DEFAULT NULL,
+  `updatedAt` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `provincia` (`provincia`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
+
+--
+-- Volcado de datos para la tabla `provincia`
+--
+
+INSERT INTO `provincia` (`provincia`, `id`, `createdAt`, `updatedAt`) VALUES
+('Andalucía', 1, NULL, NULL),
+('Aragón', 2, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `red`
 --
 
@@ -67,6 +155,19 @@ CREATE TABLE IF NOT EXISTS `redes_sociales` (
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `sesion`
+--
+
+CREATE TABLE IF NOT EXISTS `sesion` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `createdAt` datetime DEFAULT NULL,
+  `updatedAt` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `usuario`
 --
 
@@ -78,19 +179,21 @@ CREATE TABLE IF NOT EXISTS `usuario` (
   `edad` int(11) DEFAULT NULL,
   `fecha_nacimiento` date DEFAULT NULL,
   `genero` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `celebrity` tinyint(1) DEFAULT NULL,
+  `ciudad` int(11) DEFAULT NULL,
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `createdAt` datetime DEFAULT NULL,
   `updatedAt` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=13 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=2 ;
 
 --
 -- Volcado de datos para la tabla `usuario`
 --
 
-INSERT INTO `usuario` (`nombre`, `apellidos`, `email`, `password`, `edad`, `fecha_nacimiento`, `genero`, `id`, `createdAt`, `updatedAt`) VALUES
-('Jaime', 'Irazabal Gonzalezq', 'jaimeirazabal1@gmail.com', '$2a$10$z8NlFhV4YbmhqK9YGSLzBOy8pBF9Pq2BMfcAgAUZ8aok/usyVJZ2O', 29, '1986-05-27', 'H', 11, '2015-12-29 19:00:46', '2015-12-29 19:00:46');
+INSERT INTO `usuario` (`nombre`, `apellidos`, `email`, `password`, `edad`, `fecha_nacimiento`, `genero`, `celebrity`, `ciudad`, `id`, `createdAt`, `updatedAt`) VALUES
+('jaime', 'Irazabal Gonzalez', 'jaimeirazabal1@gmail.com', '$2a$10$wPz4YTcKG7zPRPx6meP2ue/JH6eGsdnjmVVYRAK4HKoRdXcUOWTrC', 29, '1986-05-27', 'H', 1, NULL, 1, '2016-01-04 14:14:06', '2016-01-04 14:14:06');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
